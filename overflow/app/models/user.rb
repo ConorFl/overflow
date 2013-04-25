76dcdfa644db7be8d3b5e48889f3a12819d87e80
Apprentice <apprentice@devbootcamp.com>
2013-04-24 21:51:54 -0700

    added user model tests

--- overflow/app/models/user.rb
+++ overflow/app/models/user.rb
@@ -2,6 +2,8 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
   attr_accessible :email, :name, :password, :password_confirmation
+  validates :name, :presence => true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   has_secure_password
   has_many :questions
   validates_confirmation_of :password
--- overflow/spec/models/user_spec.rb
+++ overflow/spec/models/user_spec.rb
@@ -2,11 +2,43 @@
 require 'spec_helper'
 
 describe User do
+
+  before(:all) do 
+    @user =User.create(:name=>"xyz",:email=>"xyz@gmail.com",:password=>"xyz",
+      :password_confirmation=>"xyz")
+    @question1 =Question.create(:title=>"xyz",:content=>"abc")
+    @question2 =Question.create(:title=>"xyz",:content=>"abc")
+    @user.questions << @question1
+    @user.questions << @question2
+    @answer1 =Answer.create(:content=>"abc")
+    @answer2 =Answer.create(:content=>"abc")
+    @question1.answers << [@answer1, @answer2]
+  end  
+
+
   it "can be instantiated" do
     User.new.should be_an_instance_of(User)
   end
 
-  it "can be saved successfully" do
-    User.create.should be_persisted
+  it "cannot be created with all nil parameters" do
+    User.create.should_not be_persisted
   end
+
+  it "user has many questions" do
+    @user.questions.length.should eq(2)
+  end
+
+  it "user enters valid email" do
+    @user2 =User.create(:name=>"xyz",:email=>"xyzmail.com",:password=>"xyz",
+      :password_confirmation=>"xyz")
+    @user2.should_not be_valid
+  end
+
+
+#PASTE IN QUESTION MODEL RSPEC
+  it "questions have answers" do
+    @user.questions.first.answers.length.should eq(2)
+  end
+
 end
+
